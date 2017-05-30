689ceaf37a6244c1045ae9a29d885649d4885c8c
afeiship <1290657123@qq.com>
2017-05-30 22:34:26 +0800

    intial & finish

---
+++ src/raf-polyfill.js
@@ -0,0 +1,18 @@
+(function () {
+
+  global = global || this;
+
+  global.requestAnimationFrame = (function () {
+    return requestAnimationFrame ||
+      global.webkitRequestAnimationFrame ||
+      global.mozRequestAnimationFrame ||
+      function (callback) {
+        global.setTimeout(callback, 1000 / 60);
+      };
+  })();
+
+  if (typeof module !== 'undefined' && module.exports) {
+    module.exports = requestAnimationFrame;
+  }
+
+}());
